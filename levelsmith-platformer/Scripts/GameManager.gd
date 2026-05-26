10c32d6dd715aae9a7a7da99fb621c7b31667b40
SteventheBeven <smp6196@rit.edu>
2026-05-26 11:50:46 -0400

    Renamed PlayScene, undid unintentional change

--- levelsmith-platformer/Scripts/GameManager.gd
+++ levelsmith-platformer/Scripts/GameManager.gd
@@ -19,7 +19,7 @@ func pause_pressed() -> void:
 		playState = PlayState.PAUSE;
 
 func reset() -> void:
-	get_tree().change_scene_to_file("res://Scenes/play_scene.tscn");
+	get_tree().change_scene_to_file("res://Scenes/PlayScene.tscn");
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:

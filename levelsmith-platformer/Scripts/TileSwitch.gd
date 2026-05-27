7c60d575fd78587af45e140952e63aa87cf7f1bb
Zay Arriaga <112396755+zayisawesomethe2nd@users.noreply.github.com>
2026-05-27 12:24:58 -0400

    Fix to UI being attached to viewport

--- levelsmith-platformer/Scripts/TileSwitch.gd
+++ levelsmith-platformer/Scripts/TileSwitch.gd
@@ -1,6 +1,6 @@
 extends HBoxContainer
 
-@onready var editorManager : Node2D = get_parent().get_parent();
+@onready var editorManager : Node2D = get_parent().get_parent().get_parent();
 
 func _on_solid_tile_button_pressed() -> void:
 	editorManager.change_tile(Global.TileType.SOLID);

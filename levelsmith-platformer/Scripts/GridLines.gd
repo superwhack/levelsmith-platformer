48cc02ce75e22356f092c7a42629ee85b4996817
Zay Arriaga <112396755+zayisawesomethe2nd@users.noreply.github.com>
2026-06-03 14:58:11 -0400

    grid lines script created

---
+++ levelsmith-platformer/Scripts/GridLines.gd
@@ -0,0 +1,18 @@
+extends TileMapLayer
+
+# Master and Editor Manager exports for easy access
+@export var masterManager : Node2D;
+@export var editorManager : Node2D;
+
+# The grid size, taken from the MasterManager.
+@onready var gridSize : Vector2i = masterManager.worldSize;
+
+## Called when the grid lines tile map layer is created.
+func _ready() -> void:
+	fill_grid_lines();
+
+## Fills the grid with grid lines tiles.
+func fill_grid_lines() -> void:
+	for height in range(0, get_parent().worldSize.y + 1):
+		for width in range(0, get_parent().worldSize.x + 1):
+			self.set_cell(Vector2i(height, width), 1, Vector2i.ZERO);

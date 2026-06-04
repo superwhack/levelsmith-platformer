6d44ac660f361de55f94ffb878f73ac0952663b0
Zay Arriaga <112396755+zayisawesomethe2nd@users.noreply.github.com>
2026-06-04 11:34:54 -0400

    move_tile made, prevTile replaced with prevEntity

--- levelsmith-platformer/Scripts/HotkeyManager.gd
+++ levelsmith-platformer/Scripts/HotkeyManager.gd
@@ -9,13 +9,13 @@ func _unhandled_key_input(event: InputEvent) -> void:
 		toolManager.rotate_tile();
 	
 	if event.is_action_pressed("brush-tool"):
-		if (toolManager.prevTile != -1):
+		if (toolManager.prevEntity != -1):
 			entityManager.drop_entity();
 		toolManager.change_tool(Global.Tool.BRUSH);
 		editorManager.change_current_hotbar(Global.HotbarState.TILES);
 
 	elif event.is_action_pressed("box-brush-tool"):
-		if (toolManager.prevTile != -1):
+		if (toolManager.prevEntity != -1):
 			entityManager.drop_entity();
 		toolManager.change_tool(Global.Tool.BOX_BRUSH);
 		editorManager.change_current_hotbar(Global.HotbarState.TILES);

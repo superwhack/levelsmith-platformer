75de4bbee69a84de76d3f47de9edda4b1397d102
SteventheBeven <smp6196@rit.edu>
2026-06-04 11:30:11 -0400

    find_image_in_folder function completed

--- levelsmith-platformer/Scripts/AssetManager.gd
+++ levelsmith-platformer/Scripts/AssetManager.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var filePath: String;
+var filePath: String = "user://Assets";
 
 # References to images
 var newImage: Image;
@@ -16,7 +16,7 @@ var audioToReplace: AudioStream;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	find_image_in_folder("Tile");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,8 +26,34 @@ func _process(delta: float) -> void:
 func find_image(imageName: String) -> Image:
 	return null;
 
+## Finds and loads the first image found in given folder
+## folderName: Name of the folder to search in
+## returns: Image loaded if it is found
 func find_image_in_folder(folderName: String) -> Image:
-	return null;
+	# Opens the folder at the given folderName path
+	var dir = DirAccess.open(filePath + "/" + folderName);
+	# If a folder was sucessfully opened
+	if (dir):
+		# Initialize file stream
+		dir.list_dir_begin();
+		# Get the image name in the folder
+		var imageName: String = dir.get_next();
+		# If there is no image in the folder, print error
+		if (imageName == ""):
+			print("No file found");
+			return null;
+		else:
+			# Initialize an image
+			var image: Image = Image.new();
+			# Load the image based on it's file path
+			image.load(filePath + "/" + folderName + "/" + imageName);
+			# Close file stream
+			dir.list_dir_end();
+			# Return loaded image
+			return image;
+	else:
+		print("Could not open file path");
+		return null;
 
 func validate_image(image: Image) -> Image:
 	return null;

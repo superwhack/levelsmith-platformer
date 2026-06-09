546fcfc72bd9a7994c7a8f56e0fea7c223a10844
Evan-Calkins <exc8424@rit.edu>
2026-06-09 12:32:06 -0400

    signal reset
    hello

--- levelsmith-platformer/Scripts/Editor/ToolSwitch.gd
+++ levelsmith-platformer/Scripts/Editor/ToolSwitch.gd
@@ -4,9 +4,9 @@ extends HBoxContainer
 @export var editorManager : Node2D;
 @export var toolManager : Node2D;
 
-func _on_brush_tool_button_pressed() -> void:
-	toolManager.change_tool(Global.Tool.BRUSH);
-	editorManager.change_current_hotbar(Global.HotbarState.TILES);
+#func _on_brush_tool_button_pressed() -> void:
+#	toolManager.change_tool(Global.Tool.BRUSH);
+#	editorManager.change_current_hotbar(Global.HotbarState.TILES);
 
 func _on_box_brush_tool_button_pressed() -> void:
 	toolManager.change_tool(Global.Tool.BOX_BRUSH);
@@ -14,3 +14,8 @@ func _on_box_brush_tool_button_pressed() -> void:
 
 func _on_cursor_button_pressed() -> void:
 	toolManager.change_tool(Global.Tool.CURSOR);
+
+
+func _on_brush_button_pressed() -> void:
+	toolManager.change_tool(Global.Tool.BRUSH);
+	editorManager.change_current_hotbar(Global.HotbarState.TILES);

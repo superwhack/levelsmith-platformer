73702ff33b45d27f43329128f53aae0e3ab88f97
NebSeniah <bwh8037@g.rit.edu>
2026-06-10 09:34:35 -0500

    Added the patrolling enemy's restricted property.

--- levelsmith-platformer/Scripts/Game/EnemyPatrol.gd
+++ levelsmith-platformer/Scripts/Game/EnemyPatrol.gd
@@ -14,6 +14,8 @@ extends Enemy
 @export var rayCastDownL : RayCast2D;
 @export var rayCastDownR : RayCast2D;
 
+@export var teleCast : RayCast2D;
+
 func _physics_process(delta: float) -> void:
 	# Gravity
 	super._physics_process(delta);
@@ -28,13 +30,16 @@ func patrol_behavior() -> void:
 	if rayCastLeft.is_colliding():
 		direction = 1;
 	# BUG: Doesn't work yet
-	if restricted && !rayCastDownL.is_colliding() && !rayCastDownR.is_colliding():
-		print("One not touching.")
+	if restricted && !(rayCastDownL.is_colliding() && rayCastDownR.is_colliding()):
 		if (!rayCastDownL.is_colliding()):
 			direction = 1;
 		elif (!rayCastDownR.is_colliding()):
 			direction = -1;
 	velocity.x = direction * groundSpeed * 400;
+	
+	# NOTE: A better solution would need to be found in order to get the enemy to stick to slopes
+	#if teleCast.is_colliding() && velocity.y > 17:
+	#	velocity.y = teleCast.get_collision_point().y;
 
 func assign_script(id: String, position: Vector2i) -> void:
 	propertyFile = load("res://Resources/Enemies/Patrol" + id + ".tres");

fee5d1c62ecb4a9055ec107360065b0a2df58e81
AndrewSH-548 <ash7811@rit.edu>
2026-06-12 11:29:24 -0400

    grid lines cleaned

--- levelsmith-platformer/Scripts/Editor/GridLines.gd
+++ levelsmith-platformer/Scripts/Editor/GridLines.gd
@@ -3,16 +3,9 @@ extends TileMapLayer
 # Master and Editor Manager exports for easy access
 @export var masterManager : Node2D;
 
-# The grid size, taken from the MasterManager.
-@onready var gridSize : Vector2i = masterManager.worldSize;
-
 ## Called when the grid lines tile map layer is created.
 func _ready() -> void:
 	fill_grid_lines();
-	
-	print("Level Height:", gridSize.y);
-	print("Level Width:", gridSize.x);
-
 
 ## Fills the grid with grid lines tiles.
 func fill_grid_lines() -> void:

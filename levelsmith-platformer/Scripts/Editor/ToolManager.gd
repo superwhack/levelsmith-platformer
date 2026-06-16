d5e9f545c197a04421acb85f9b35eec5de109867
NebSeniah <bwh8037@g.rit.edu>
2026-06-16 14:18:07 -0500

    Moving entities over tiles

--- levelsmith-platformer/Scripts/Editor/ToolManager.gd
+++ levelsmith-platformer/Scripts/Editor/ToolManager.gd
@@ -20,6 +20,7 @@ var boxBrushState: Global.BoxBrushState = Global.BoxBrushState.INACTIVE
 # The previously selected tile before dragging
 var prevEntity : int = -1;
 var prevRotation : int = 0;
+var prevPosition: Vector2;
 var brushObject: int = 0;
 
 # A timer to differentiate between click and holding click

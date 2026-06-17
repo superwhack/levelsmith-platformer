108ff7d96603c6c4450529f3ad6edb00a92dd6f0
AndrewSH-548 <ash7811@rit.edu>
2026-06-17 14:35:31 -0400

    Flying stuff re-added

--- levelsmith-platformer/Scripts/Editor/EntityManager.gd
+++ levelsmith-platformer/Scripts/Editor/EntityManager.gd
@@ -71,6 +71,10 @@ func place_entity(clickPosition: Vector2) -> void:
 				get_scene_at_cell(clickPosition).adjust_arrow(90);
 				get_scene_at_cell(clickPosition).directionArrow.scale = Vector2(1, 1);
 				ResourceSaver.save(newShooting, "res://Resources/Enemies/Shooting" + str(time) + ".tres");
+			elif (saveBrush == Global.EntityType.FLYING):
+				var defaultFlying: Resource = load("res://Resources/PlayerPresets/FlyingDefault.tres");
+				var newFlying: Resource = defaultFlying.duplicate(true);
+				ResourceSaver.save(newFlying, "res://Resources/Enemies/Flying" + str(time) + ".tres");
 			get_scene_at_cell(clickPosition).assign_script(str(time), clickPosition);
 		else:
 			tileSet.set_cell(clickPosition, toolManager.brushObject, Vector2i.ZERO, 1);

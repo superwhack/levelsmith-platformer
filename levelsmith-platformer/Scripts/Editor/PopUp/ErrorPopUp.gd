ad97bf91034f23e2ca57abbab0fd9660c1be94c6
AndrewSH-548 <ash7811@rit.edu>
2026-06-18 12:15:39 -0400

    error popup signals fixed

--- levelsmith-platformer/Scripts/Editor/PopUp/ErrorPopUp.gd
+++ levelsmith-platformer/Scripts/Editor/PopUp/ErrorPopUp.gd
@@ -1,6 +1,12 @@
 extends Panel
 
+@export var closeButton: Button;
+
+## Runs when the node is first created
+func _ready() -> void:
+	closeButton.pressed.connect(close_popup);
+
 ## Closes popup/clears from stack
-func closePopUp() -> void:
+func close_popup() -> void:
 	queue_free();
 	# Additional functionality can be added below

c8be4b8f35c2c076d530fa20cf8c8a19037ae810
AndrewSH-548 <ash7811@rit.edu>
2026-06-22 12:36:25 -0400

    player property editing fixed

--- levelsmith-platformer/Scripts/Editor/Properties/PropertySlider.gd
+++ levelsmith-platformer/Scripts/Editor/Properties/PropertySlider.gd
@@ -22,10 +22,10 @@ func _ready() -> void:
 	slider.min_value = snapped(minMax.x, 0.01);
 	slider.max_value = snapped(minMax.y, 0.01);
 	slider.step = sliderStep;
-	slider.drag_ended.connect(drag_ended.emit);
+	slider.drag_ended.connect(_drag_ended);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var newLabel = "";
 	if int(sliderStep) == sliderStep:
 		newLabel += str(int(slider.value));
@@ -35,6 +35,10 @@ func _process(delta: float) -> void:
 		newLabel += valueAppend;
 	label.text = newLabel;
 	value = slider.value;
+
+## When drag is finished, emit drag ended signal
+func _drag_ended() -> void:
+	drag_ended.emit();
 
 ## Update the slider value
 func update_slider() -> void:

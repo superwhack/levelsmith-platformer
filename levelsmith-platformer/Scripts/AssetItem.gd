bcdcad7c65d78bfb1ea60ca85ea5b09a7aea3638
Zay Arriaga <112396755+zayisawesomethe2nd@users.noreply.github.com>
2026-06-23 11:37:48 -0400

    AssetItem

--- levelsmith-platformer/Scripts/AssetItem.gd
+++ levelsmith-platformer/Scripts/AssetItem.gd
@@ -1,18 +1,23 @@
 extends Button
 class_name AssetItem;
+
+# Exported variable for display and asset names.
 @export var displayName: String;
 @export var assetName: String;
 
+# Enum for the type of asset being modified.
 enum AssetType { IMAGE, ANIMATION, AUDIO };
 @export var type: AssetType;
 
+# An signal for when an item is chosen.
 signal item_selected(selectedItem: AssetItem);
 
-# Called when the node enters the scene tree for the first time.
+## Set the text and connect appropriate signals on ready.
 func _ready() -> void:
 	text = displayName;
 	pressed.connect(item_selected.emit);
 	focus_exited.connect(on_focus_exited);
 
+## When the asset item is unfocused, reset the modulate.
 func on_focus_exited() -> void:
 	modulate = Color(1, 1 , 1, 1);

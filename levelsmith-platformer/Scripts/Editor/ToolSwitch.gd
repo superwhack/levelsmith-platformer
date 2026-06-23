37205a13ffb4b3e9d1876ed838fe7e20b9e5d599
SteventheBeven <smp6196@rit.edu>
2026-06-23 12:24:24 -0400

    ToolSwitch commented

--- levelsmith-platformer/Scripts/Editor/ToolSwitch.gd
+++ levelsmith-platformer/Scripts/Editor/ToolSwitch.gd
@@ -9,20 +9,23 @@ extends HBoxContainer
 @export var boxBrushButton : Button;
 @export var cursorButton : Button;
 
-# Runs when the node is first created. Used for connecting signals.
+## Runs when the node is first created. Used for connecting signals.
 func _ready() -> void:
 	brushButton.pressed.connect(swap_to_brush);
 	boxBrushButton.pressed.connect(swap_to_box_brush);
 	cursorButton.pressed.connect(swap_to_cursor);
 
+## Swaps currently selected tool to the brush
 func swap_to_brush() -> void:
 	toolManager.change_tool(Global.Tool.BRUSH);
 	editorManager.change_current_hotbar(Global.HotbarState.TILES);
 
+## Swaps currently selected tool to the box brush
 func swap_to_box_brush() -> void:
 	toolManager.change_tool(Global.Tool.BOX_BRUSH);
 	editorManager.change_current_hotbar(Global.HotbarState.TILES);
 
+## Swaps currently selected tool to the cursor
 func swap_to_cursor() -> void:
 	toolManager.change_tool(Global.Tool.CURSOR);
 	editorManager.change_current_hotbar(Global.HotbarState.ENTITIES);

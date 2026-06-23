edb547a7b833210312abba6efbb37a11084c5164
SteventheBeven <smp6196@rit.edu>
2026-06-23 10:58:30 -0400

    tile bar button commented

--- levelsmith-platformer/Scripts/Editor/TilebarButton.gd
+++ levelsmith-platformer/Scripts/Editor/TilebarButton.gd
@@ -1,9 +1,12 @@
 extends Button
 
-@export var thisItemID: int;
-@export var tilebar: HBoxContainer;
-
-@onready var tileSet: TileSet = tilebar.tileMap.tile_set;
+# SourceID of this item
+@export var thisItemID : int;
+# Reference to the container this item is in
+@export var tilebar : HBoxContainer;
+# Reference to the tile set
+@onready var tileSet : TileSet = tilebar.tileMap.tile_set;
 
 func _process(delta: float) -> void:
+	# Change the texture to the texture currently set in the tile set
 	icon = tileSet.get_source(thisItemID).texture;

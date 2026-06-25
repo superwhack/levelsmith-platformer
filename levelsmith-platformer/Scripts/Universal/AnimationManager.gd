4e353f2ac31a55c3d9d1dfe9535969402ec36896
SteventheBeven <smp6196@rit.edu>
2026-06-25 11:04:46 -0400

    AnimationManager in progress

--- levelsmith-platformer/Scripts/Universal/AnimationManager.gd
+++ levelsmith-platformer/Scripts/Universal/AnimationManager.gd
@@ -13,7 +13,10 @@ var stationaryEnemySprites : Array[AnimatedSprite2D];
 @export var mainTileMap : TileMapLayer;
 
 func replace_animation(animatedSprite : AnimatedSprite2D, animationName : String, frames : Array[Image]) -> void:
-	pass;
+	var spriteFrames : SpriteFrames = animatedSprite.sprite_frames;
+	spriteFrames.clear(animationName);
+	for frame in frames:
+		spriteFrames.add_frame(animationName, ImageTexture.create_from_image(frame));
 
 func refresh_animations() -> void:
 	pass;
@@ -25,4 +28,6 @@ func play_all_animations() -> void:
 	pass;
 
 func get_all_sprites() -> void:
-	pass;
+	var playerNode = get_tree().get_first_node_in_group("Player");
+	playerSprite = playerNode.find_child("AnimatedSprite2D");
+	for enemy in get_tree().get_nodes_in_group("Enemy");

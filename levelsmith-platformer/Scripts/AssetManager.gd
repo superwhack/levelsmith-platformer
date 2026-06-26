7f4a005e6e5718ffa37b004877d75659a4bc26ef
SteventheBeven <smp6196@rit.edu>
2026-06-26 12:32:03 -0400

    commented

--- levelsmith-platformer/Scripts/AssetManager.gd
+++ levelsmith-platformer/Scripts/AssetManager.gd
@@ -8,11 +8,20 @@ var filePath : String = "user://Assets";
 var imageToReplace : Image;
 var imageNameToReplace : String;
 
+# The currently selected entity types
 var selectedEntityType : String;
+
+# The Texture that will be replacing the texture with the preview name
 var animationPreviewToReplace : Texture2D;
 var animationPreviewNameToReplace : String;
+
+# The current animation index being viewed
 var currentAnimationIndex : int = 0;
+
+# The current frame the animation is on
 var animationFrameIndex : int = 0;
+
+# The currently selected item and its corresponding animation
 var currentSelectedItem : AssetItem;
 var currentLoadedAnimation : Array[Texture2D];
 
@@ -45,6 +54,7 @@ var audioToReplace : AudioStream;
 # Reference to the editor manager
 @export var editorManager : Node2D;
 
+# References to animation preview controls and visuals
 @export var animationPreviewRightButton : Button;
 @export var animationPreviewLeftButton : Button;
 @export var animationName : Label;
@@ -52,6 +62,8 @@ var audioToReplace : AudioStream;
 @export var frameLeftButton : Button;
 @export var frameCountLabel : Label;
 @export var playButton : Button;
+
+# Information about played animation
 var playingAnimation : bool;
 var FPS : float = 12;
 var animTimer : float = 0;
@@ -118,6 +130,7 @@ func _ready() -> void:
 	ImportExportManager.levelImported.connect(item_selected);
 
 func _process(delta: float) -> void:
+	# Play the animation
 	if (playingAnimation):
 		animTimer += delta;
 		if (animTimer >= 1/FPS):
@@ -146,6 +159,7 @@ func generate_buttons(folder: String, container: VBoxContainer, type: AssetItem.
 			container.add_child(newButton);
 			newButton.pressed.connect(item_selected.bind(newButton));
 			newButton.type = type;
+			# Set the default image and animation to be selected
 			if (type == AssetItem.AssetType.IMAGE && !firstImageSelected):
 				firstImageSelected = newButton;
 			if (type == AssetItem.AssetType.ANIMATION && !firstAnimationSelected):
@@ -296,17 +310,18 @@ func replace_image(newImagePath: String) -> void:
 	else:
 		imagePreviewTexture.texture = ImageTexture.create_from_image(find_image(imageNameToReplace + ".png", "res://Assets/Defaults"));
 
+## Replaces the currently previewed animation with one chosen via file dialog.
+## newAnimationPath: The path to the folder selected
 func replace_animation(newAnimationPath : String) -> void:
 	var targetFilePath : String = FileSearch.find_directory_by_name(animationPreviewNameToReplace);
 	var targetDirectory : DirAccess = clear_image(animationPreviewNameToReplace);
-	print (targetDirectory)
-	
+	# Count for which file is being iterated at for naming purposes
 	var fileCount : int = 0;
+	# Loop through every file at the path
 	for file in DirAccess.get_files_at(newAnimationPath):
 		var currentFilePath = newAnimationPath + "/" + file;
-		print(currentFilePath);
+		# If the file is a png, increase the file count and create a copy in the assets folder with its number
 		if (file.get_extension().to_lower() == "png"):
-			print("correcto")
 			fileCount += 1;
 			targetDirectory.copy(currentFilePath, str(targetFilePath, "/", animationPreviewNameToReplace, fileCount, ".png"));
 		else:
@@ -314,6 +329,7 @@ func replace_animation(newAnimationPath : String) -> void:
 				PopUpManager.create_error_popup("File type incorrect", "File must be .png format.");
 	
 	refresh_assets();
+	# Replace the preview image if there is one, if not load default
 	var replacementImage : Image = find_image_in_folder(targetFilePath);
 	if (replacementImage):
 		update_animation_preview();
@@ -322,17 +338,20 @@ func replace_animation(newAnimationPath : String) -> void:
 #func replace_audio(audioToReplace: AudioStream, newAudio: AudioStream) -> void:
 #	pass;
 
+## Clears the image in a given folder and replaces it with a default
 func reset_image() -> void:
 	clear_image(imageNameToReplace);
 	refresh_assets();
 	imagePreviewTexture.texture = ImageTexture.create_from_image(find_image(imageNameToReplace + ".png", "res://Assets/Defaults"));
 
+## Resets everything within the assets manager
 func reset_all() -> void:
 	FileSearch.delete_folder(filePath);
 	create_file_tree();
 	reset_menu();
 	refresh_assets();
 
+## Deletes and regenerates all buttons
 func reset_menu() -> void:
 	for button: Button in imagesTab.get_children():
 		button.queue_free();
@@ -356,7 +375,9 @@ func reset_menu() -> void:
 ## Signal that is emitted when an asset in the menu is selected
 ## selectedItem: The item that is selected, defaults to the firstImageSelected
 func item_selected(selectedItem: AssetItem = firstImageSelected) -> void:
+	# Pause the animation
 	playingAnimation = false;
+	# If the selected item is an image, replace its preview
 	if (selectedItem.type == AssetItem.AssetType.IMAGE):
 		imageNameToReplace = selectedItem.assetName;
 		imageToReplace = find_image_in_folder(FileSearch.find_directory_by_name(imageNameToReplace));
@@ -366,6 +387,7 @@ func item_selected(selectedItem: AssetItem = firstImageSelected) -> void:
 				imagePreviewTexture.texture = ImageTexture.create_from_image(imageToReplace);
 		else:
 			imagePreviewTexture.texture = ImageTexture.create_from_image(find_image(imageNameToReplace + ".png", "res://Assets/Defaults"));
+	# If the selected image is an animation, reset the frame and load the animation into currentlyLoadedAnimation
 	elif (selectedItem.type == AssetItem.AssetType.ANIMATION):
 		currentAnimationIndex = 0;
 		animationFrameIndex = 0;
@@ -378,36 +400,49 @@ func item_selected(selectedItem: AssetItem = firstImageSelected) -> void:
 	currentAssetLabel.text = selectedItem.displayName;
 	currentSelectedItem = selectedItem;
 
+## Switch between animations within the entity
+## next: Whether the user is switching to the next or previous animation
 func anim_change(next : bool):
+	# Pause the animation
 	playingAnimation = false;
+	# Increase or decrease the index accordingly
 	if (next):
 		currentAnimationIndex += 1;
 	else:
 		currentAnimationIndex -= 1;
+	# Find the amount of animations the entity has
 	var animationsCount : int = DirAccess.get_directories_at(FileSearch.find_directory_by_name(selectedEntityType)).size();
+	# Clamp the animation index within how many the entity has
 	if (currentAnimationIndex >= animationsCount):
 		currentAnimationIndex = 0;
 	elif (currentAnimationIndex < 0):
 		currentAnimationIndex = animationsCount - 1;
+	# Set the frame to 0
 	animationFrameIndex = 0;
 	animationPreviewNameToReplace = DirAccess.get_directories_at(FileSearch.find_directory_by_name(selectedEntityType))[currentAnimationIndex];
+	# Load the current animation
 	currentLoadedAnimation.clear();
 	for image in get_animation_from_folder(animationPreviewNameToReplace):
 		currentLoadedAnimation.append(ImageTexture.create_from_image(image));
 	update_animation_preview();
 
+## Change the animation frame currently shown
+## next: Whether the user is changing to the next or previous frame
 func frame_change(next : bool = true):
+	# Change the animation frame index accordingly
 	if (next):
 		animationFrameIndex += 1;
 	else:
 		animationFrameIndex -= 1;
 	var frameCount : int = currentLoadedAnimation.size();
+	# Clamp the current index between 0 and the currently loaded animation amount
 	if (animationFrameIndex >= frameCount):
 		animationFrameIndex = 0;
 	elif (animationFrameIndex < 0):
 		animationFrameIndex = frameCount - 1;
 	update_animation_preview();
 
+## Update all parts of the animation preview
 func update_animation_preview() -> void:
 	animationName.text = animationPreviewNameToReplace;
 	var frameCount = currentLoadedAnimation.size();

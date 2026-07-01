0bbe68b627120bde655f577a467f8a3fd5f0e8f5
NebSeniah <bwh8037@g.rit.edu>
2026-07-01 13:03:12 -0500

    Added returnclick stuff
    When loading a level you don't have to click once to be able to place entities anymore

--- levelsmith-platformer/Scripts/Universal/MasterManager.gd
+++ levelsmith-platformer/Scripts/Universal/MasterManager.gd
@@ -77,6 +77,7 @@ func level_setup( levelName: String, newSize: Vector2i ) -> void:
 	#AudioManager.update_volume();
 	#print("NEW LEVEL SET UP");
 	Global.levelCreated.emit();
+	editorManager.returnClick = false;
 
 func create_bedrock_border() -> void:
 	for x in range(-1, worldSize.x + 1):
@@ -100,6 +101,7 @@ func import_level_and_edit() -> void:
 		childNode.free();
 	editorManager.playerExists = await ImportExportManager.import_level_CSV(editorManager.tileMap);
 	worldSize = ImportExportManager.importedLevelSize;
+	editorManager.returnClick = false;
 	entityManager.scan_goals(worldSize.x, worldSize.y);
 	editorManager.reset_enemy_positions();
 	await get_tree().process_frame;

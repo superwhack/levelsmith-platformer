7bf91ae7d780dd4ea6961d45b6d826b7c7fc6e37
NebSeniah <bwh8037@g.rit.edu>
2026-07-02 12:10:53 -0500

    Property Slider
    Min + Max added
    Text field added
    Still need to work on restricting it

--- levelsmith-platformer/Scripts/Editor/Properties/PropertySlider.gd
+++ levelsmith-platformer/Scripts/Editor/Properties/PropertySlider.gd
@@ -4,7 +4,9 @@ extends VBoxContainer
 @export var propertyName : String;
 @export var nameLabel : Label; 
 @export var slider : HSlider;
-@export var label : Label;
+@export var textField : TextEdit;
+@export var minLabel : Label;
+@export var maxLabel : Label;
 
 @export var minMax : Vector2;
 @export var sliderStep : float;
@@ -24,11 +26,21 @@ func _ready() -> void:
 	slider.max_value = snapped(minMax.y, 0.01);
 	slider.step = sliderStep;
 	slider.drag_ended.connect(_drag_ended);
+	textField.focus_entered.connect(_text_change_begins);
+	textField.focus_exited.connect(_text_change_ended);
+	minLabel.text = str(snapped(minMax.x, .01));
+	maxLabel.text = str(snapped(minMax.y, .01));
+	# Just so _process runs once
+	value = slider.value + 1;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("enter"):
+		textField.release_focus();
 	if !enabled:
 		slider.value = value;
+		return;
+	if value == slider.value:
 		return;
 	var newLabel = "";
 	if int(sliderStep) == sliderStep:
@@ -37,11 +49,20 @@ func _process(_delta: float) -> void:
 		newLabel += str(slider.value);
 	if valueAppend:
 		newLabel += valueAppend;
-	label.text = newLabel;
+	textField.text = newLabel;
 	value = slider.value;
 
 ## When drag is finished, emit drag ended signal
 func _drag_ended(_value_changed: bool) -> void:
+	drag_ended.emit();
+	
+func _text_change_begins() -> void:
+	textField.placeholder_text = textField.text;
+	textField.text = "";
+
+func _text_change_ended() -> void:
+	slider.value = float(textField.text);
+	textField.text = "";
 	drag_ended.emit();
 
 ## Update the slider value

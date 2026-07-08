a1f144c72ca2bbb32a7d00c4ab9205a2ce9fce3f
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2026-07-08 15:53:20 -0400

    projectiles choose sprite based on function

--- levelsmith-platformer/Scripts/Game/Projectile.gd
+++ levelsmith-platformer/Scripts/Game/Projectile.gd
@@ -3,15 +3,26 @@ extends Area2D
 # Reference to the VisibleOnScreenEnabler
 @export var onScreen : VisibleOnScreenEnabler2D;
 
+# Reference to child Sprite2D
+@export var sprite : Sprite2D;
+# Reverences to Texture2D assets for bullet variants
+@export var dangerTexture : Texture2D;
+@export var bounceTexture : Texture2D;
+
 #var direction : float;
 var speed : float;
 # Whether or not the projectile can be bounced on
 var bounceable : bool;
 
-## When the scene enters the tree, connect important signals
+## When the scene enters the tree, assign proper texture and connect important signals
 func _ready() -> void:
 	body_entered.connect(delete_projectile);
 	onScreen.screen_exited.connect(delete_projectile);
+	
+	if bounceable:
+		sprite.texture = bounceTexture
+	else:
+		sprite.texture = dangerTexture
 
 ## Move the projectile at a speed
 func _process(delta: float) -> void:

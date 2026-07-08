5ccebffa570c25255d7a4019eface8bccb1ba27a
SteventheBeven <smp6196@rit.edu>
2026-07-08 14:16:41 -0400

    Tilebar button visual updates

--- levelsmith-platformer/Scripts/Editor/TilebarButton.gd
+++ levelsmith-platformer/Scripts/Editor/TilebarButton.gd
@@ -9,12 +9,22 @@ extends Button
 # Reference to the tile set
 @onready var tileSet : TileSet = tilebar.tileMap.tile_set;
 
+@export var isEntity : bool = false;
+
+@export var entityName : String = "";
+
 func _ready() -> void:
 	pressed.connect(change_brush_object)
 
 func _process(_delta: float) -> void:
 	# Change the texture to the texture currently set in the tile set
-	if (isTextureUpdating): icon = tileSet.get_source(thisItemID).texture;
+	if (isTextureUpdating): 
+		if(isEntity && entityName != ""):
+			var templateAnimation : AnimatedSprite2D = AnimationManager.get(entityName + "TemplateSprite");
+			icon = templateAnimation.sprite_frames.get_frame_texture(templateAnimation.animation, 0);
+		else:
+			icon = tileSet.get_source(thisItemID).texture;
+	
 
 func change_brush_object() -> void:
 	AudioManager.play_UI_effect("UI_Selection");

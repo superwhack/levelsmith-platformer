b8fa896970e81595265f860cd6492104c38e0c8f
NebSeniah <bwh8037@g.rit.edu>
2026-07-10 10:29:43 -0500

    Changed wording
    Shouldn't really alter functionality in any serious way, just looks clearer

--- levelsmith-platformer/Scripts/Game/Player.gd
+++ levelsmith-platformer/Scripts/Game/Player.gd
@@ -383,7 +383,7 @@ func detect_tiles() -> void:
 					velocity.y *= .94;
 				if tileName != "slow":
 					currentSlowdown = 1.0;
-			if Input.is_action_just_pressed("jump") && velocity.y != 0:
+			if Input.is_action_just_pressed("jump") && !is_on_floor():
 				# Depending on direction, apply a different x velocity
 				if rayDirection.x < 0:
 					if wallJumpDirection != WallDirection.LEFT:

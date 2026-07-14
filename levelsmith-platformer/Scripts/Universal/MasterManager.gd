548985145dbd222699cff2f67635083a65304ee9
Zay Arriaga <112396755+zayisawesomethe2nd@users.noreply.github.com>
2026-07-14 13:47:42 -0400

    fixed import/load issue

--- levelsmith-platformer/Scripts/Universal/MasterManager.gd
+++ levelsmith-platformer/Scripts/Universal/MasterManager.gd
@@ -74,7 +74,10 @@ func _ready() -> void:
 		DirAccess.make_dir_absolute("user://Resources/Enemies/");
 		DirAccess.copy_absolute("res://Resources/PlayerPresets/Default.tres", "user://Resources/Custom.tres");
 		
+	main_menu(false);
+	
 	await screen_static();
+	await main_menu(false, true);
 
 
 ## When the level is completed, validate it and automatically return to editor

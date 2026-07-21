69047f7ba4fe103bce31e917e9cd7eafaa5c9777
NebSeniah <bwh8037@g.rit.edu>
2026-07-21 10:56:42 -0500

    Preset Scripts

--- levelsmith-platformer/Scripts/Editor/Properties/PlayerMovementPreset.gd
+++ levelsmith-platformer/Scripts/Editor/Properties/PlayerMovementPreset.gd
@@ -1,20 +1,38 @@
 extends Resource
-## A class that contains variables for different player movement stats, 
-## such as ground speed or jumping height.
 class_name PlayerMovementPreset
 
-# Variables for player movement stats
+# Player's maximum health
 @export var health : int;
+
+# Player's groundspeed when walking
 @export var groundSpeed : float;
+
+# Player's ac/deceleration, affects how slowly they begin/stop moving
 @export var acceleration : float;
 @export var deceleration : float;
+
+# Jump height affects jump height
 @export var jumpHeight : float;
+
+# `Friction` in air, makes it harder to turn in midair when lower
 @export var airControl : float;
+
+# Fall speed determines maximum fall speed
 @export var fallSpeed : float;
+
+# How long the player can be off the ground and still jump
 @export var coyoteTime : float;
 
+# True if slopes affect speed like usual with a CharacterBody2D
 @export var slopeSlowdown : bool;
+
+# True if the player can drop through oneways
 @export var oneways : bool;
+
+# True if the player can double jump
 @export var doubleJump : bool;
+
+# True if player can wall jump
 @export var wallJump : bool;
+# True if player's wall jump decays when jumping off the same side repeatedly
 @export var wallJumpDecay : bool;

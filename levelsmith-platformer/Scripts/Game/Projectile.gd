965509e7098cd2d297791765700d972cb0370bb9
SteventheBeven <smp6196@rit.edu>
2026-07-21 13:27:05 -0400

    Projectile up to standard

--- levelsmith-platformer/Scripts/Game/Projectile.gd
+++ levelsmith-platformer/Scripts/Game/Projectile.gd
@@ -6,8 +6,9 @@ extends Area2D
 # Reference to child Sprite2D
 @export var sprite : Sprite2D;
 
-#var direction : float;
+# Speed that the projectile moves
 var speed : float;
+
 # Whether or not the projectile can be bounced on
 var bounceable : bool;
 
@@ -20,12 +21,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	global_position += transform.x * delta * speed * 100;
 
+## Assign the sprite's texture
+## texture : The given texture for the projectile
 func assign_texture(texture: Texture2D) -> void:
 	sprite.texture = texture;
 
-## Delete this projectile once it's offscreen
+## Delete this projectile once it's offscreen or it hits something
 func delete_projectile(body: Node2D = null) -> void:
-	if body == null || body is MovingPlatform:
+	if (body == null || body is MovingPlatform):
 		queue_free();
 	elif body is TileMapLayer:
 		# STRETCH : If we can figure out how to get the proper position of the tile, this can be uncommented to let projectiles pass on the underside of one ways

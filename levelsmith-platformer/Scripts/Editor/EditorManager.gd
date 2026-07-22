cff7569397b0a0a3278f588fb6fc027254526b17
chrish04 <145590450+chris00004@users.noreply.github.com>
2026-07-22 14:47:58 -0400

    random direction icon reset fixed

--- levelsmith-platformer/Scripts/Editor/EditorManager.gd
+++ levelsmith-platformer/Scripts/Editor/EditorManager.gd
@@ -120,8 +120,10 @@ func reset_enemy_positions() -> void:
 			if (!moving is EnemyFlyer):
 				moving.global_position += Vector2(0, 20);
 			
-			if (moving is EnemyPatrol || moving is EnemyShooting):
+			if (moving is EnemyPatrol):
 				moving.directionArrow.show();
+			elif (moving is EnemyShooting):
+				moving.directionArrow.visible = !moving.randomDirection;
 			elif (moving is EnemyFlyer):
 				moving.previewLine.show();
 			elif (moving is EnemyStationary):

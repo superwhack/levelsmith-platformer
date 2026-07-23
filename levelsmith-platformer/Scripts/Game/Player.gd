231977e531883d4448a176948cd484d429f1914f
NebSeniah <bwh8037@g.rit.edu>
2026-07-23 13:02:38 -0500

    Revert Change

--- levelsmith-platformer/Scripts/Game/Player.gd
+++ levelsmith-platformer/Scripts/Game/Player.gd
@@ -735,7 +735,9 @@ func detect_tiles() -> void:
 		var tileLayer : TileMapLayer = collider;
 		
 		var hitGlobal : Vector2 = raycast.get_collision_point();
-		var probeLocal : Vector2 = tileLayer.to_local(hitGlobal);
+		var hitNormal : Vector2 = raycast.get_collision_normal();
+		var probeGlobal : Vector2 = hitGlobal - hitNormal * 0.5;
+		var probeLocal : Vector2 = tileLayer.to_local(probeGlobal);
 		var tilePos : Vector2i = tileLayer.local_to_map(probeLocal);
 		var tileData : TileData = tileLayer.get_cell_tile_data(tilePos);
 		

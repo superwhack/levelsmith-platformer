b5331b6ac91acc2d1c301a8efa9539ea5d60c5b3
chrish04 <145590450+chris00004@users.noreply.github.com>
2026-07-24 12:42:56 -0400

    few more tweaks for wall jump height feel

--- levelsmith-platformer/Scripts/Game/Player.gd
+++ levelsmith-platformer/Scripts/Game/Player.gd
@@ -730,7 +730,11 @@ func wall_jump():
 		velocity.x *= currentFriction;
 	if airControl != 1:
 		velocity.x *= pow(airControl, WALL_JUMP_AIR_CONTROL_POWER);
-	velocity.y = -WALL_JUMP_FORCE_Y * jumpHeight * sqrt(1.0 / wallJumpCount) * sqrt(fallSpeed) / pow(clamp(groundSpeed, WALL_JUMP_Y_GROUND_MIN, WALL_JUMP_Y_GROUND_MAX), WALL_JUMP_SPEED_EXPONENT_Y);
+	var combinedWallJumpHeightMax = 7.5;
+	var wallJumpHeight = jumpHeight + wallJumpStrength/1.15;
+	if ( wallJumpHeight > combinedWallJumpHeightMax ) :
+		wallJumpHeight = combinedWallJumpHeightMax ;
+	velocity.y = -WALL_JUMP_FORCE_Y * wallJumpHeight * sqrt(1.0 / wallJumpCount) * sqrt(fallSpeed) / pow(clamp(groundSpeed, WALL_JUMP_Y_GROUND_MIN, WALL_JUMP_Y_GROUND_MAX), WALL_JUMP_SPEED_EXPONENT_Y);
 	
 	# Slow down on slow tiles (and on ice, but you normally wall jump faster anyways)
 	if (tileName == "slow" || tileName == "ice"):
